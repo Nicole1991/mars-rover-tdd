01c61ac2d814ffb9ec3cdcf4d791975e66c6679e
qyzhao <qyzhao@thoughtworks.com>
2020-01-12 12:13:24 +0800

    [qy] init commit for mars-rover study

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void should_return_true_when_given_true() {
+        assertEquals(true, true);
+    }
+}
