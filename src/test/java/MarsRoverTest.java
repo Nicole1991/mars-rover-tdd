3c865ecc9284434fe5b375adb7901ecfe49dd2f0
qyzhao <qyzhao@thoughtworks.com>
2020-01-13 14:27:18 +0800

    [qy] add test about marsRover get move instruction should y minus 1 when current direction is S

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -23,4 +23,13 @@ public class MarsRoverTest {
         Location newLocation = marsRover.executionMove(location);
         assertEquals(2, newLocation.getY());
     }
+
+    @Test
+    public void should_return_0_nagetive_1_S_when_initial_location_is_0_0_S_and_accept_move_instruction() {
+        MarsRover marsRover = new MarsRover();
+        Location location = new Location(0, 0, Direction.S);
+
+        Location newLocation = marsRover.executionMove(location);
+        assertEquals(-1, newLocation.getY());
+    }
 }
