f36d565cd43d2a50aa78976db1cee3ad1ba37127
qyzhao <qyzhao@thoughtworks.com>
2020-01-13 14:32:11 +0800

    [qy] refactor: should test executionInstruction method

--- src/main/java/main/MarsRover.java
+++ src/main/java/main/MarsRover.java
@@ -2,10 +2,20 @@ package main;
 
 import entity.Location;
 import enums.Direction;
+import enums.Instruction;
 
 public class MarsRover {
+    private Location location;
 
-    public Location executionMove(Location location) {
+    public MarsRover(Location location) {
+        this.location = location;
+    }
+
+    public Location executionInstruction(Instruction instruction) {
+        return executionMove(this.location);
+    }
+
+    private Location executionMove(Location location) {
         int currentY = location.getY();
         if (location.getDirection() == Direction.N) {
             location.setY(currentY + 1);
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,5 +1,6 @@
 import entity.Location;
 import enums.Direction;
+import enums.Instruction;
 import main.MarsRover;
 import org.junit.Test;
 
@@ -8,28 +9,28 @@ import static org.junit.Assert.assertEquals;
 public class MarsRoverTest {
     @Test
     public void should_return_0_1_N_when_initial_location_is_0_0_N_and_accept_move_instruction() {
-        MarsRover marsRover = new MarsRover();
         Location location = new Location(0, 0, Direction.N);
+        MarsRover marsRover = new MarsRover(location);
 
-        Location newLocation = marsRover.executionMove(location);
+        Location newLocation = marsRover.executionInstruction(Instruction.M);
         assertEquals(1, newLocation.getY());
     }
 
     @Test
     public void should_return_0_2_N_when_initial_location_is_0_1_N_and_accept_move_instruction() {
-        MarsRover marsRover = new MarsRover();
         Location location = new Location(0, 1, Direction.N);
+        MarsRover marsRover = new MarsRover(location);
 
-        Location newLocation = marsRover.executionMove(location);
+        Location newLocation = marsRover.executionInstruction(Instruction.M);
         assertEquals(2, newLocation.getY());
     }
 
     @Test
     public void should_return_0_nagetive_1_S_when_initial_location_is_0_0_S_and_accept_move_instruction() {
-        MarsRover marsRover = new MarsRover();
         Location location = new Location(0, 0, Direction.S);
+        MarsRover marsRover = new MarsRover(location);
 
-        Location newLocation = marsRover.executionMove(location);
+        Location newLocation = marsRover.executionInstruction(Instruction.M);
         assertEquals(-1, newLocation.getY());
     }
 }
