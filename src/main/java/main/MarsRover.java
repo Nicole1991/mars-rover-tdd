d55753e87c3dac3a3ddc684d5b2505a84594bec0
qyzhao <qyzhao@thoughtworks.com>
2020-01-13 14:18:43 +0800

    [qy] implement about marsRover get move instruction should y add 1 use fastest way

--- src/main/java/main/MarsRover.java
+++ src/main/java/main/MarsRover.java
@@ -1,11 +1,12 @@
 package main;
 
 import entity.Location;
+import enums.Direction;
 
 public class MarsRover {
     private Location location;
 
     public Location executionMove(Location location) {
-        return new Location();
+        return new Location(0, 1, Direction.N);
     }
 }
