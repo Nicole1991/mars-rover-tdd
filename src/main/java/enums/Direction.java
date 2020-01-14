568bde74315c85a9b266f589f39e021eeb8476a2
qyzhao <qyzhao@thoughtworks.com>
2020-01-14 15:27:45 +0800

    [qy] add test about marsRover get move instruction with current direction E should x add 1

--- src/main/java/enums/Direction.java
+++ src/main/java/enums/Direction.java
@@ -2,5 +2,6 @@ package enums;
 
 public enum Direction {
     N,
+    E,
     S,
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,6 +1,7 @@
 import main.MarsRover;
 import org.junit.Test;
 
+import static enums.Direction.E;
 import static enums.Direction.N;
 import static enums.Direction.S;
 import static org.junit.Assert.assertEquals;
@@ -8,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 public class MarsRoverTest {
     @Test
     public void should_return_1_1_N_when_given_1_0_N_and_execute_instruction_move() {
-        MarsRover marsRover = new MarsRover(1, 0 , N);
+        MarsRover marsRover = new MarsRover(1, 0, N);
 
         marsRover.executeInstruction("M");
 
@@ -26,5 +27,16 @@ public class MarsRoverTest {
         assertEquals(1, marsRover.getX());
         assertEquals(S, marsRover.getDirection());
         assertEquals(1, marsRover.getY());
+    }
+
+    @Test
+    public void should_return_2_1_E_when_given_1_1_E_and_execute_instruction_move() {
+        MarsRover marsRover = new MarsRover(1, 1, E);
+
+        marsRover.executeInstruction("M");
+
+        assertEquals(1, marsRover.getX());
+        assertEquals(E, marsRover.getDirection());
+        assertEquals(2, marsRover.getY());
     }
 }
